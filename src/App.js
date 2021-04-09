edc1fb344cae7f56fec45b4d71ffe9ca41be0f42
TanushaVeraksa <54898478+TanushaVeraksa@users.noreply.github.com>
2021-04-09 15:14:44 +0300

    Change color and counting click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Site from './Site'
+import ClickCounter from './Site'
 
 class App extends React.Component {
 constructor(props) {
@@ -10,14 +10,14 @@ constructor(props) {
 }
 
 handleClick = () => {
-
       this.setState({counter: this.state.counter + 1})
 }
 
   render() {
     return (
   <>
-    <Site name= {this.state.counter}></Site>
+    <ClickCounter number= {this.state.counter}></ClickCounter>
+    <p> counter: {this.state.counter}</p>
     <button onClick={this.handleClick}>counter</button>
   </>
     );
--- src/Site.js
+++ src/Site.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-export default function Site ({name}) {
-    return ( <div>Counter {name > 5 ? ":)" : name}</div> 
+export default function ClickCounter ({number}) {
+     const redColor = {color: "red"};
+     const greenColor = {color: "green"};
+    return ( <>
+        <div>Counter {number < 2 ? "новичок" : number < 11 ? "умелый" :
+                     number < 101 ? "мастер": "человек со большим количеством свободного времени" }
+        </div> 
+        <p>{number% 2 == 0 ? <span style = {greenColor}>Color</span> : <span style = {redColor}>Color</span>}</p> 
+        </>
     )
 }
